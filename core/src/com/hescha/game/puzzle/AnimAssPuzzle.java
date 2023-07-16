afb6dfecd20c9ea98e6ad2b69818058dcd4535ea
Yauhescha <evgescha.maslov007@yandex.ru>
2023-07-17 00:35:49 +0300

    Add main screen to game

--- core/src/com/hescha/game/puzzle/AnimAssPuzzle.java
+++ core/src/com/hescha/game/puzzle/AnimAssPuzzle.java
@@ -5,13 +5,13 @@ import com.hescha.game.puzzle.screen.GameScreen;
 import com.hescha.game.puzzle.screen.MainMenuScreen;
 
 public class AnimAssPuzzle extends Game {
-    public static final float WORLD_WIDTH = 512;
+    public static final float WORLD_WIDTH = 720;
     public static final float WORLD_HEIGHT = 1280;
     public static AnimAssPuzzle launcher;
 
     @Override
     public void create() {
         launcher = this;
-        setScreen(new GameScreen());
+        setScreen(new MainMenuScreen());
     }
 }
--- core/src/com/hescha/game/puzzle/screen/MainMenuScreen.java
+++ core/src/com/hescha/game/puzzle/screen/MainMenuScreen.java
@@ -1,77 +1,96 @@
 package com.hescha.game.puzzle.screen;
 
+import static com.hescha.game.puzzle.AnimAssPuzzle.WORLD_HEIGHT;
+import static com.hescha.game.puzzle.AnimAssPuzzle.WORLD_WIDTH;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.hescha.game.puzzle.AnimAssPuzzle;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.hescha.game.puzzle.util.FontUtil;
 
 public class MainMenuScreen extends ScreenAdapter {
     Stage stage;
     BitmapFont font;
-    TextButton.TextButtonStyle skin;
+    ImageTextButton.ImageTextButtonStyle textButtonUpStule;
     Table table;
-    private Color backgroundColor;
+    Color backgroundColor;
+
+
+    Texture backgroundTexture;
+    Texture headerTexture;
+    Texture buttonTexture;
+    Texture mainImage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    SpriteBatch batch;
 
     @Override
     public void show() {
+        camera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT);
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        camera.update();
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        viewport.apply(true);
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture(Gdx.files.internal("ui/1 MainMenu.png"));
+        buttonTexture = new Texture(Gdx.files.internal("ui/button.png"));
+        headerTexture = new Texture(Gdx.files.internal("ui/header.png"));
+        mainImage = new Texture(Gdx.files.internal("ui/MainImage.png"));
+
         table = new Table();
         table.setFillParent(true);
+        font = FontUtil.generateFont(Color.BLACK);
 
-        stage = new Stage();
+        TextureRegion headerCover = new TextureRegion(headerTexture);
+        TextureRegionDrawable buttonDrawable0 = new TextureRegionDrawable(headerCover);
+        ImageTextButton imageTextButton0 = new ImageTextButton("PUZZLE", new ImageTextButton.ImageTextButtonStyle(buttonDrawable0, null, null, font));
+        table.add(imageTextButton0).center().padTop(10).padBottom(10).row();
+
+        Image mainimage = new Image(mainImage);
+        table.add(mainimage).center().padTop(10).padBottom(10).row();
+
+        TextureRegion btnPlay = new TextureRegion(buttonTexture);
+        TextureRegionDrawable buttonDrawable1 = new TextureRegionDrawable(btnPlay);
+        ImageTextButton imageTextButton1 = new ImageTextButton("PLAY", new ImageTextButton.ImageTextButtonStyle(buttonDrawable1, null, null, font));
+        table.add(imageTextButton1).center().padTop(10).padBottom(10).row();
+
+        TextureRegion bthGallery = new TextureRegion(buttonTexture);
+        TextureRegionDrawable buttonDrawable2 = new TextureRegionDrawable(bthGallery);
+        ImageTextButton imageTextButton2 = new ImageTextButton("GALLERY", new ImageTextButton.ImageTextButtonStyle(buttonDrawable2, null, null, font));
+        table.add(imageTextButton2).center().padTop(10).padBottom(10).row();
+
+        TextureRegion bthExit = new TextureRegion(buttonTexture);
+        TextureRegionDrawable buttonDrawable3 = new TextureRegionDrawable(bthExit);
+        ImageTextButton imageTextButton3 = new ImageTextButton("EXIT", new ImageTextButton.ImageTextButtonStyle(buttonDrawable3, null, null, font));
+        table.add(imageTextButton3).center().padTop(10).padBottom(10).row();
+
+
+
+        stage = new Stage(viewport);
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
 
-        font = FontUtil.generateFont(Color.BLACK);
-
-        skin = new TextButton.TextButtonStyle();
-        skin.font = font;
-        skin.fontColor = Color.BLACK;
-
-        prepareActionButton();
-
-        backgroundColor = Color.WHITE;
-    }
-
-    private void prepareActionButton() {
-        table.reset();
-        Table innerTable = new Table();
-
-            String buttonName = "Play";
-            TextButton button = new TextButton(buttonName, skin);
-            innerTable.add(button).pad(100).row();
-            button.addListener(new ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    AnimAssPuzzle.launcher.setScreen(new GameScreen());
-                }
-            });
-
-        TextButton exit = new TextButton("Exit", skin);
-        innerTable.add(exit).pad(100).row();
-        exit.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                Gdx.app.exit();
-            }
-        });
-
-        ScrollPane scrollPane = new ScrollPane(innerTable);
-        table.add(scrollPane);
+        backgroundColor =  new Color(245f/255,232f/255,194f/255,1);
     }
 
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(backgroundColor);
+
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
@@ -80,5 +99,10 @@ public class MainMenuScreen extends ScreenAdapter {
     public void dispose() {
         font.dispose();
         stage.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 }
